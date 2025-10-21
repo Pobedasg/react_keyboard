6feb37b238d3bfbd8ffc4aac3f2539f53821d653
Victoriia <kod.victoriya@gmail.com>
2025-10-21 21:45:54 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import classNames from 'classnames';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type KeyLoggerState = {
+  lastPressedKey: string | null;
+};
+
+export class App extends Component<{}, KeyLoggerState> {
+  state: KeyLoggerState = {
+    lastPressedKey: null,
+  };
+
+  handleKeyUp = (event: KeyboardEvent) => {
+    this.setState({ lastPressedKey: event.key });
+    console.log(event.key);
+  };
+
+  componentDidMount() {
+    document.addEventListener('keyup', this.handleKeyUp as any);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyUp as any);
+  }
+
+  render() {
+    const { lastPressedKey } = this.state;
+    const message =
+      lastPressedKey === null
+        ? 'Nothing was pressed yet'
+        : `The last pressed key is [${lastPressedKey}]`;
+
+    return (
+      <div className={classNames('key-logger')}>
+        <p>{message}</p>
+      </div>
+    );
+  }
+}
